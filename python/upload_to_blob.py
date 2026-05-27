90ccdca1b6da0b42121496adb71c2f4be55d31e2
Chithra E P <chithraep1@gmail.com>
2026-05-27 20:52:10 +0100

    Added Gold Layer

--- python/upload_to_blob.py
+++ python/upload_to_blob.py
@@ -2,31 +2,27 @@
 from azure.storage.blob import BlobServiceClient
 import os
 
-# Azure Storage connection string
-connection_string = "connection string"
+#Azure connection string
+connection_string="my connection string"
 
-# Connect to Blob Storage
-blob_service_client=BlobServiceClient.from_connection_string(connection_string)
+#Connect to Azure Blob
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
+#containers and folders
+folders={
+    "silver":"data/silver",
+    "gold":"data/gold"
+}
 
-# Container name
-container_name = "silver"
+#upload files
+for container_name ,local_folder in folders.items():
+    for file_name in os.listdir(local_folder):
+        file_path=os.path.join(local_folder,file_name)
 
-# Local folder path
-local_folder = "data/silver"
-
-# Upload files
-for file_name in os.listdir(local_folder):
-
-    file_path = os.path.join(local_folder, file_name)
-
-    blob_client = blob_service_client.get_blob_client(
-        container=container_name,
-        blob=file_name
-    )
-
-    with open(file_path, "rb") as data:
-        blob_client.upload_blob(data, overwrite=True)
-
-    print(f"{file_name} uploaded successfully")
-
+        blob_client=blob_service_client.get_blob_client(
+            container=container_name,
+            blob=file_name)
+        
+        with open(file_path,"rb") as data:
+            blob_client.upload_blob(data,overwrite=True)
+        print(f"{file_name} uploaded to {container_name}")
