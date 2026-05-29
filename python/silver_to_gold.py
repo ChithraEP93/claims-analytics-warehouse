1709a7ee1d5bd1b9e04d9844c20fc7bd2c252093
Chithra E P <chithraep1@gmail.com>
2026-05-29 18:51:16 +0100

    addedAdded gold analytics for claims status and risk rating

--- python/silver_to_gold.py
+++ python/silver_to_gold.py
@@ -50,9 +50,39 @@ def generate_claims_by_status():
         .agg(["count","sum","mean"])
         .reset_index()
     )
+    status_df.columns=["claim_status","total_claims",
+    "total_claim_amount",
+    "average_claim_amount"]
     #save gold file
-    df.to_csv("data/gold/claims_claims_by_status",index=False)
+    status_df.to_csv("data/gold/claims_by_status",index=False)
     print("Claims by status created")
+
+
+def generate_claims_by_risk_rating():
+    #Read cleaned claims
+    cl_df=pd.read_csv("data/silver/cleaned_claims.csv")
+    #Read cleaned coberholders
+    cv_df=pd.read_csv("data/silver/cleaned_coverholders.csv")
+
+
+    #merge 
+    merged_df=cl_df.merge(cv_df,on="coverholder_id")
+
+    #Group By Coverholder's risk rating
+    risk_df=(
+        merged_df.groupby("risk_rating")
+        ["claim_amount"].agg(["count","sum","mean"])
+        .reset_index())
+    
+    risk_df.columns=["risk_rating",
+    "total_claims",
+    "total_claim_amount",
+    "average_claim_amount"]
+    
+    #save file
+    risk_df.to_csv("data/gold/claims_by_Risk.csv",index=False)
+    print("Claims by risk rating created")
 
 generate_claims_kpis()
 generate_claims_by_status()
+generate_claims_by_risk_rating()
